0cd31486fec41ebe938396e589ca358c20864ab3
Shivang-18 <91329329+Shivang-18@users.noreply.github.com>
2024-11-19 21:41:47 +0800

    Update API_sms_twilio.py

--- API_sms_twilio.py
+++ API_sms_twilio.py
@@ -10,10 +10,10 @@ from twilio.rest import Client
 # In[3]:
 
 def message_ward():
-    acc_sid = "AC3cda6ea5fd57703f4e8cbec75767fb36"
-    auth_program = "597dc57a12cbc744644c18b28499bb40"
-    twilio_num = "+14847598258"
-    target_num="+919650229309"
+    acc_sid = "ACXXXXXXXXXXXXXXXXXXXb36"
+    auth_program = "597XXXXXXXXXXXXXXXXXXX0"
+    twilio_num = "+14XXXXXXXXXX"
+    target_num="+91XXXXXXXXXX"
     
     client=Client(acc_sid,auth_program)
 
